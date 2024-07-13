22f4db84eff0b559d768a9e0a9bc60391d792c26
24-ManPatel <manpatel240406@gmail.com>
2024-07-13 21:56:44 +0530

    Something fishy

--- client/src/components/CyberYodha.jsx
+++ client/src/components/CyberYodha.jsx
@@ -12,10 +12,12 @@ const CyberYoda = () => {
             <br></br>
             <br></br>
             <br></br>
-            <p className="title">IOT Security</p>
           </div>
           <br></br>
           <p className="description">
+          IOT Security
+          <br></br>
+          <br></br>
             We understand the unprecedented opportunities that the Internet of  Things (IoT) brings to businesses. However, we also recognize the critical importance of securing your IoT ecosystem against evolving cyber threats.
           </p>
         </div>
